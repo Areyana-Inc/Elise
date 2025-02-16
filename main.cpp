eca77842e5c1cdf2110c4b2da5fdab78472be9cf
Ilya Demidovich <Ilya.Demidovich@kaspersky.com>
2025-02-16 17:18:24 +0300

    add teal theme + icons

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+
+    QDir::addSearchPath("icon", QDir::currentPath() + "/theme");
+      
+    QFile file("dark_teal.qss");
+    file.open(QFile::ReadOnly);
+
+    QString styleSheet { file.readAll() };
+    app.setStyleSheet(styleSheet);
+
     MainWindow w;
     w.show();
-    return a.exec();
+    return app.exec();
 }
